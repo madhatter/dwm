3083a1bd36fb4dce2cd383ce87e9e2cfea7f1ae0
Arvid Warnecke <madhatter@nostalgix.org>
2013-03-26 05:49:40 +0100

    Switching keybindings for enter key

--- config.h
+++ config.h
@@ -104,7 +104,7 @@ static const char *prev[]       = { "mpc", "prev", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = lockcmd } },		
 	{ MODKEY,                       XK_n,      spawn,          {.v = ncmpcppcmd } },
 	{ MODKEY|ShiftMask,				XK_Up,     spawn,		   {.v = volup } },
@@ -119,7 +119,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
