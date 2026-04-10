048a4cd9ab8b1b539cddee671369f8d626ca7edb
Arvid Warnecke <madhatter@nostalgix.org>
2026-04-10 18:05:34 +0200

    Updated config.h for 2026 ;)

--- config.h
+++ config.h
@@ -2,13 +2,15 @@
 
 /* Customized config.h */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "monospace:size=10" };
-static const char *fonts[]          = { "DejaVuSansMono Nerd Font Mono:size=12" };
+static const char *fonts[]          = { "DejaVuSansMono Nerd Font Mono:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -39,13 +41,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask    iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,         1 << 6,        0,           1,         -1 }, // appears on tag 7
-	{ "URxvt",    NULL,       "ncmpcpp",    1 << 5,        1,           1,         -1 },
-	{ "URxvt",    NULL,       "ncspot",     1 << 5,        1,           1,         -1 },
-	{ "keepassx", NULL,       NULL,         1 << 7,        0,           1,         -1 },
-	{ "Spotify",  NULL,       NULL,         1 << 5,        0,           1,         -1 }, // fixes rendering glitches
-	{ "Steam",    NULL,       NULL,         1 << 6,        0,           1,         -1 }, // fixes rendering glitches
-	{ "jetbrains-idea",  NULL,   NULL,      1 << 1,        0,           1,         -1 }, // fixes rendering glitches
+	{ "jetbrains-idea",  NULL,   NULL,      1 << 1,        0,           1,         -1 }, // appears on tag 2, floating
+	{ "firefox",  NULL,       NULL,         1 << 3,        0,           0,         -1 }, // appears on tag 4
+	{ "elecwhat",	NULL,       NULL,         1 << 4,        1,           1,         -1 }, // appears on tag 5, floating, centered
+	{ "discord",  NULL,       NULL,         1 << 4,        0,           1,         -1 }, // appears on tag 5, floating
+	{ "Alacritty",NULL,       "ncmpcpp",    1 << 5,        1,           1,         -1 }, // appears on tag 6, floating, centered
+	{ "Spotify",  NULL,       NULL,         1 << 5,        0,           1,         -1 }, // appears on tag 6, floating
+	{ "steam",    NULL,       NULL,         1 << 6,        0,           1,         -1 }, // appears on tag 7, floating
+	{ "Gimp",     NULL,       NULL,         1 << 7,        0,           1,         -1 }, // appears on tag 8, floating
 };
 
 /* layout(s) */
@@ -76,18 +79,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-//static const char *dmenucmd[] = { "/home/awarnecke/bin/dmenu_bind.sh", NULL };
 static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const char *lockcmd[]	= { "slock", NULL };
-static const char *ncmpcppcmd[] = { "urxvt", "-title", "ncmpcpp", "-e", "ncmpcpp", NULL };
-static const char *ncspotcmd[] = { "urxvt", "-title", "ncspot", "-e", "/home/awarnecke/.cargo/bin/ncspot", NULL };
+static const char *ncmpcppcmd[] = { "alacritty", "--title", "ncmpcpp", "-e", "ncmpcpp", NULL };
 
-static const char *voldown[]    = { "amixer", "-q", "set", "Master", "2%-",  NULL };
-static const char *voltoggle[]  = { "pactl", "set-sink-mute", "0", "toggle",  NULL };
-static const char *volup[]      = { "amixer", "-q", "set", "Master", "2%+",  NULL };
+static const char *voldown[]    = { "wpctl", "set-volume", "-l", "1.0", "@DEFAULT_AUDIO_SINK@","5%-",  NULL };
+static const char *volup[]      = { "wpctl", "set-volume", "-l", "1.0", "@DEFAULT_AUDIO_SINK@","5%+",  NULL };
+static const char *voltoggle[]  = { "wpctl", "set-mute",   "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
 
 static const char *brightup[]   = { "sudo", "light", "-A", "5", NULL};
 static const char *brightdown[] = { "sudo", "light", "-U", "5", NULL};
@@ -96,25 +96,27 @@ static const char *next[]       = { "mpc", "next", NULL };
 static const char *play[]       = { "mpc", "toggle", NULL };
 static const char *prev[]       = { "mpc", "prev", NULL };
 
-static const char *snext[]		= { "spotifycli", "--next", NULL };
-static const char *splay[]		= { "spotifycli", "--playpause", NULL };
-static const char *sprev[]		= { "spotifycli", "--prev", NULL };
+static const char *snext[]		= { "playerctl", "next", NULL };
+static const char *splay[]		= { "playerctl", "play-pause", NULL };
+static const char *sprev[]		= { "playerctl", "previous", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0,                            0x1008ff03,spawn,          {.v = brightdown} },
-	{ 0,                            0x1008ff02,spawn,          {.v = brightup} },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn,          {.v = brightdown} },
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn,          {.v = brightup} },
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_n,      spawn,          {.v = ncmpcppcmd } },
-	{ MODKEY,                       XK_s,      spawn,          {.v = ncspotcmd } },
 	{ MODKEY|ShiftMask,             XK_Up,     spawn,          {.v = volup } },
-	{ 0,                            0x1008ff13,spawn,          {.v = volup } },
 	{ MODKEY|ShiftMask,             XK_Down,   spawn,          {.v = voldown } },
-	{ 0,                            0x1008ff11,spawn,          {.v = voldown } },
 	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = voltoggle } },
-	{ 0,                            0x1008ff12,spawn,          {.v = voltoggle } },
+	/* Vim-style Volume controls */
+	{ MODKEY|ShiftMask,             XK_k,      spawn,          {.v = volup   } },
+	{ MODKEY|ShiftMask,             XK_j,      spawn,          {.v = voldown } },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,          {.v = volup   } },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,          {.v = voldown } },
+	{ 0,                            XF86XK_AudioMute,         spawn,          {.v = voltoggle } },
 	{ MODKEY|ShiftMask,             XK_Left,   spawn,          {.v = prev } },
 	{ MODKEY|ShiftMask,             XK_Right,  spawn,          {.v = next } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = play } },
@@ -152,7 +154,6 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
